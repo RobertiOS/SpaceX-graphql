8edfcf61a226d73244f68c5a78d10b789aaa1f13
Robert <robert.ios.developer@gmail.com>
2022-11-28 22:28:28 -0600

    :card_file_box: chore: add new query and fragment

--- FruitsGraphQL/Networking/APIManager/API.swift
+++ FruitsGraphQL/Networking/APIManager/API.swift
@@ -15,18 +15,19 @@ public final class LaunchListQuery: GraphQLQuery {
         hasMore
         launches {
           __typename
-          id
-          site
-          mission {
-            __typename
-            missionPatch
-          }
+          ...LaunchDetails
         }
       }
     }
     """
 
   public let operationName: String = "LaunchList"
+
+  public var queryDocument: String {
+    var document: String = operationDefinition
+    document.append("\n" + LaunchDetails.fragmentDefinition)
+    return document
+  }
 
   public var after: String?
 
@@ -43,7 +44,7 @@ public final class LaunchListQuery: GraphQLQuery {
 
     public static var selections: [GraphQLSelection] {
       return [
-        GraphQLField("launches", arguments: ["after": GraphQLVariable("after")], type: .nonNull(.object(Launch.selections)))
+        GraphQLField("launches", arguments: ["after": GraphQLVariable("after")], type: .nonNull(.object(Launch.selections))),
       ]
     }
 
@@ -74,7 +75,7 @@ public final class LaunchListQuery: GraphQLQuery {
           GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
           GraphQLField("cursor", type: .nonNull(.scalar(String.self))),
           GraphQLField("hasMore", type: .nonNull(.scalar(Bool.self))),
-          GraphQLField("launches", type: .nonNull(.list(.object(Launch.selections))))
+          GraphQLField("launches", type: .nonNull(.list(.object(Launch.selections)))),
         ]
       }
 
@@ -130,9 +131,7 @@ public final class LaunchListQuery: GraphQLQuery {
         public static var selections: [GraphQLSelection] {
           return [
             GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-            GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
-            GraphQLField("site", type: .scalar(String.self)),
-            GraphQLField("mission", type: .object(Mission.selections))
+            GraphQLFragmentSpread(LaunchDetails.self),
           ]
         }
 
@@ -140,10 +139,6 @@ public final class LaunchListQuery: GraphQLQuery {
 
         public init(unsafeResultMap: ResultMap) {
           self.resultMap = unsafeResultMap
-        }
-
-        public init(id: GraphQLID, site: String? = nil, mission: Mission? = nil) {
-          self.init(unsafeResultMap: ["__typename": "Launch", "id": id, "site": site, "mission": mission.flatMap { (value: Mission) -> ResultMap in value.resultMap }])
         }
 
         public var __typename: String {
@@ -155,71 +150,319 @@ public final class LaunchListQuery: GraphQLQuery {
           }
         }
 
-        public var id: GraphQLID {
+        public var fragments: Fragments {
           get {
-            return resultMap["id"]! as! GraphQLID
+            return Fragments(unsafeResultMap: resultMap)
           }
           set {
-            resultMap.updateValue(newValue, forKey: "id")
+            resultMap += newValue.resultMap
           }
         }
 
-        public var site: String? {
-          get {
-            return resultMap["site"] as? String
-          }
-          set {
-            resultMap.updateValue(newValue, forKey: "site")
-          }
-        }
-
-        public var mission: Mission? {
-          get {
-            return (resultMap["mission"] as? ResultMap).flatMap { Mission(unsafeResultMap: $0) }
-          }
-          set {
-            resultMap.updateValue(newValue?.resultMap, forKey: "mission")
-          }
-        }
-
-        public struct Mission: GraphQLSelectionSet {
-          public static let possibleTypes: [String] = ["Mission"]
-
-          public static var selections: [GraphQLSelection] {
-            return [
-              GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-              GraphQLField("missionPatch", type: .scalar(String.self))
-            ]
-          }
-
+        public struct Fragments {
           public private(set) var resultMap: ResultMap
 
           public init(unsafeResultMap: ResultMap) {
             self.resultMap = unsafeResultMap
           }
 
-          public init(missionPatch: String? = nil) {
-            self.init(unsafeResultMap: ["__typename": "Mission", "missionPatch": missionPatch])
-          }
-
-          public var __typename: String {
+          public var launchDetails: LaunchDetails {
             get {
-              return resultMap["__typename"]! as! String
+              return LaunchDetails(unsafeResultMap: resultMap)
             }
             set {
-              resultMap.updateValue(newValue, forKey: "__typename")
-            }
-          }
-
-          public var missionPatch: String? {
-            get {
-              return resultMap["missionPatch"] as? String
-            }
-            set {
-              resultMap.updateValue(newValue, forKey: "missionPatch")
+              resultMap += newValue.resultMap
             }
           }
         }
+      }
+    }
+  }
+}
+
+public final class LaunchQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query Launch($launchId: ID!) {
+      launch(id: $launchId) {
+        __typename
+        ...LaunchDetails
+      }
+    }
+    """
+
+  public let operationName: String = "Launch"
+
+  public var queryDocument: String {
+    var document: String = operationDefinition
+    document.append("\n" + LaunchDetails.fragmentDefinition)
+    return document
+  }
+
+  public var launchId: GraphQLID
+
+  public init(launchId: GraphQLID) {
+    self.launchId = launchId
+  }
+
+  public var variables: GraphQLMap? {
+    return ["launchId": launchId]
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("launch", arguments: ["id": GraphQLVariable("launchId")], type: .object(Launch.selections)),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(launch: Launch? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Query", "launch": launch.flatMap { (value: Launch) -> ResultMap in value.resultMap }])
+    }
+
+    public var launch: Launch? {
+      get {
+        return (resultMap["launch"] as? ResultMap).flatMap { Launch(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "launch")
+      }
+    }
+
+    public struct Launch: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Launch"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLFragmentSpread(LaunchDetails.self),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var fragments: Fragments {
+        get {
+          return Fragments(unsafeResultMap: resultMap)
+        }
+        set {
+          resultMap += newValue.resultMap
+        }
+      }
+
+      public struct Fragments {
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public var launchDetails: LaunchDetails {
+          get {
+            return LaunchDetails(unsafeResultMap: resultMap)
+          }
+          set {
+            resultMap += newValue.resultMap
+          }
+        }
+      }
+    }
+  }
+}
+
+public struct LaunchDetails: GraphQLFragment {
+  /// The raw GraphQL definition of this fragment.
+  public static let fragmentDefinition: String =
+    """
+    fragment LaunchDetails on Launch {
+      __typename
+      id
+      site
+      mission {
+        __typename
+        missionPatch
+        name
+      }
+      rocket {
+        __typename
+        name
+      }
+    }
+    """
+
+  public static let possibleTypes: [String] = ["Launch"]
+
+  public static var selections: [GraphQLSelection] {
+    return [
+      GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+      GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+      GraphQLField("site", type: .scalar(String.self)),
+      GraphQLField("mission", type: .object(Mission.selections)),
+      GraphQLField("rocket", type: .object(Rocket.selections)),
+    ]
+  }
+
+  public private(set) var resultMap: ResultMap
+
+  public init(unsafeResultMap: ResultMap) {
+    self.resultMap = unsafeResultMap
+  }
+
+  public init(id: GraphQLID, site: String? = nil, mission: Mission? = nil, rocket: Rocket? = nil) {
+    self.init(unsafeResultMap: ["__typename": "Launch", "id": id, "site": site, "mission": mission.flatMap { (value: Mission) -> ResultMap in value.resultMap }, "rocket": rocket.flatMap { (value: Rocket) -> ResultMap in value.resultMap }])
+  }
+
+  public var __typename: String {
+    get {
+      return resultMap["__typename"]! as! String
+    }
+    set {
+      resultMap.updateValue(newValue, forKey: "__typename")
+    }
+  }
+
+  public var id: GraphQLID {
+    get {
+      return resultMap["id"]! as! GraphQLID
+    }
+    set {
+      resultMap.updateValue(newValue, forKey: "id")
+    }
+  }
+
+  public var site: String? {
+    get {
+      return resultMap["site"] as? String
+    }
+    set {
+      resultMap.updateValue(newValue, forKey: "site")
+    }
+  }
+
+  public var mission: Mission? {
+    get {
+      return (resultMap["mission"] as? ResultMap).flatMap { Mission(unsafeResultMap: $0) }
+    }
+    set {
+      resultMap.updateValue(newValue?.resultMap, forKey: "mission")
+    }
+  }
+
+  public var rocket: Rocket? {
+    get {
+      return (resultMap["rocket"] as? ResultMap).flatMap { Rocket(unsafeResultMap: $0) }
+    }
+    set {
+      resultMap.updateValue(newValue?.resultMap, forKey: "rocket")
+    }
+  }
+
+  public struct Mission: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Mission"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("missionPatch", type: .scalar(String.self)),
+        GraphQLField("name", type: .scalar(String.self)),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(missionPatch: String? = nil, name: String? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Mission", "missionPatch": missionPatch, "name": name])
+    }
+
+    public var __typename: String {
+      get {
+        return resultMap["__typename"]! as! String
+      }
+      set {
+        resultMap.updateValue(newValue, forKey: "__typename")
+      }
+    }
+
+    public var missionPatch: String? {
+      get {
+        return resultMap["missionPatch"] as? String
+      }
+      set {
+        resultMap.updateValue(newValue, forKey: "missionPatch")
+      }
+    }
+
+    public var name: String? {
+      get {
+        return resultMap["name"] as? String
+      }
+      set {
+        resultMap.updateValue(newValue, forKey: "name")
+      }
+    }
+  }
+
+  public struct Rocket: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Rocket"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+        GraphQLField("name", type: .scalar(String.self)),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(name: String? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Rocket", "name": name])
+    }
+
+    public var __typename: String {
+      get {
+        return resultMap["__typename"]! as! String
+      }
+      set {
+        resultMap.updateValue(newValue, forKey: "__typename")
+      }
+    }
+
+    public var name: String? {
+      get {
+        return resultMap["name"] as? String
+      }
+      set {
+        resultMap.updateValue(newValue, forKey: "name")
       }
     }
   }
